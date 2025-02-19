c1e33d183c0912afa9995a2a909c47f20ea929e3
bigaddict <davidnjihia536@gmail.com>
2025-02-19 18:35:36 +0300

    `Updated production settings to use environment variables and updated branding in templates`

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,6 +1,24 @@
 from .base import *
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["bigaddict.shop", "www.bigaddict.shop"]
+
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 try:
     from .local import *
